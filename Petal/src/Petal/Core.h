3c3e290e0e85c17267a7d0f826fda9c25c6ded3e
Oliver Bailey Smith <smitholiverbailey@gmail.com>
2023-04-04 11:09:58 -0400

    Add asserts

--- Petal/src/Petal/Core.h
+++ Petal/src/Petal/Core.h
@@ -7,5 +7,13 @@
 		#define PTL_API __declspec(dllimport)
 	#endif
 #else
-	#error Petal only supports windows!
+	#error Petal only supports Windows!
+#endif
+
+#ifdef PTL_ENABLE_ASSERTS
+	#define PTL_ASSERT(x, ...) { if(!(x)) { PTL_CRITICAL("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define PTL_CORE_ASSERT(x, ...) { if(!(x)) { PTL_CORE_CRITICAL("Assertion failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define PTL_ASSERT(x, ...)
+	#define PTL_CORE_ASSERT(x, ...)
 #endif
